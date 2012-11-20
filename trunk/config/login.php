29d0b0ed9f52be7561885a91db76aaececd5a908
rizkynugraha92@gmail.com <rizkynugraha92@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-11-20 22:18:47 +0000

    fungsi login

---
+++ trunk/config/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<div class="container">
+		<form class="form-horizontal" method+"post" action="">
+			
+				<legend>Login</legend>
+			
+		</form>
+		
+	</div>
+
+<body>
