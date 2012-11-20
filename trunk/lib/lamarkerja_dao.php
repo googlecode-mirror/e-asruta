e44d844b926540c19b342a8910339c659a15bc32
djepits5005@gmail.com <djepits5005@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-11-20 15:51:01 +0000

    Penambahan file lamarkerja_dao.php

---
+++ trunk/lib/lamarkerja_dao.php
@@ -0,0 +1,19 @@
+<?php
+include '../config/koneksi.php';
+include 'lamarkerja.php';
+
+class LamarKerja_Dao{
+		
+	function lihatDaftarLamaran(){
+	}
+	
+	function submitlamaran(){
+	}
+	
+	function editLamaran(){
+	}
+	
+	function hapusLamaran(){
+	}
+	
+}
