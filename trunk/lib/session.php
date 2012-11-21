d8c537fc75c114b801fccccba62dc4a9ffdeecde
eclipsebeat@gmail.com <eclipsebeat@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-11-21 03:22:29 +0000

    menambah kelas session

--- trunk/lib/session.php
+++ trunk/lib/session.php
@@ -10,7 +10,36 @@
       $koneksi->connect();
 			$sql = mysql_query("select kd_user where username='$username' and password='$password'");
 			$user_data = mysql_fetch_array($sql);
-      echo $user_data['name'];
+      $no_rows = mysql_num_rows($result);
+		if ($no_rows == 1) 
+        {
+           $_SESSION['login'] = true;
+           $_SESSION['username'] = $user_data['username'];
+           return TRUE;
+        }
+        else
+        {
+           return FALSE;
+        }
+		}
+      
+    // Ambil nama
+    public function get_fullname($kd_user){
+        $result = mysql_query("SELECT username FROM auths WHERE kd_user = $kd_user");
+        $user_data = mysql_fetch_array($result);
+        echo $user_data['name'];
+    }
+    
+    // session 
+    public function get_session(){
+        return $_SESSION['login'];
+    }
+    
+    // Logout 
+	public function user_logout() 
+		{
+		$_SESSION['login'] = FALSE;
+		session_destroy();
 		}
 	}
 ?>
