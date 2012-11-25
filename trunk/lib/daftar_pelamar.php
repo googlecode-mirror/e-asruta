26b0210f3b961ea07bdead2e03235d7e358c0054
eclipsebeat@gmail.com <eclipsebeat@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-11-25 12:52:40 +0000

    kelas dan function untuk mencari daftar lamaran pekerjaan

---
+++ trunk/lib/daftar_pelamar.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author cccccc
+ * @copyright 2012
+ */
+
+class daftar_pelamar{
+    var $id_asisten;
+    var $id_lamar;
+    var $keterampilan;
+    var $gaji;
+    var $lokasi;
+}
+
+?>
