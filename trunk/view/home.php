8fd910bead872681dc1d1df03cebf97a9a2e3e32
rizkynugraha92@gmail.com <rizkynugraha92@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-11-26 15:52:37 +0000

    desain home sementara

---
+++ trunk/view/home.php
@@ -0,0 +1,82 @@
+<html lang="en">
+
+<head>
+<title>coba </title>
+<img src='img/first call logo.JPG' width='300'. height='100' />
+  <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="css\bootstrap-responsive.css"></link>
+<link rel="stylesheet" href="css\bootstrap.min.css"></link>
+<link rel="stylesheet" href="css\bootstrap-responsive.min.css"></link>
+<form class="form-inline pull-right">
+  <input type="text" class="input-small" placeholder="Email">
+  <input type="password" class="input-small" placeholder="Password">
+  <label class="checkbox">
+    <input type="checkbox"> Remember me
+  </label>
+  <button type="submit" class="btn">Sign in</button>
+</form>
+
+
+
+
+</head>
+<body>
+<link rel="stylesheet/less" href="/path/to/bootstrap.less">
+<div class="container-fluid">
+  <div class="row-fluid">
+    <div class="span2">
+      <!--Sidebar content-->
+	  <h1>&nbsp;</h1>
+	  <h1>&nbsp;</h1>
+	  <ul class="nav nav-list">
+  <li class="nav-header"><h2>Regional</h2></li>
+  <li class="active"><a href="#">Home</a></li>
+  <li><a href="#">Library</a></li>
+  ...
+</ul>
+    </div>
+    <div class="span10">
+      <!--Body content-->
+	  <div class="navbar navbar-inverse">  
+  <div class="navbar-inner">  
+    <div class="container">
+	<div class="nav-collapse">
+      <ul class="nav"> 
+        <li class="active">  
+    <a class="brand" href="home.php">Homepage</a>  
+  </li>  
+</ul>  
+<ul class="nav nav-pills">  
+    <li class="dropdown all-camera-dropdown">  
+           <a class="dropdown-toggle" data-toggle="dropdown" href="#">  
+            TO-DO 
+               <b class="caret"></b>  
+           </a>  
+    <ul class="dropdown-menu">  
+            <li data-filter-camera-type="all"><a data-toggle="tab" href="TO-DO view.php">View</a></li>  
+            <li data-filter-camera-type="Alpha"><a data-toggle="tab" href="TO-DO-add.php">Add</a></li>  
+            <li data-filter-camera-type="Zed"><a data-toggle="tab" href="to-do-edit.php">Edit</a></li>  
+            <li data-filter-camera-type="Bravo"><a data-toggle="tab" href="#">Delete</a></li>  
+  
+     </ul>  
+</li></ul>  
+<form class="form-search pull-right">
+  <input type="text" class="input-medium search-query">
+  <button type="submit" class="btn">Search</button>
+</form>
+
+
+
+    <script type="text/javascript" src="js/tests/vendor/jquery.js"></script>  
+  <script type="text/javascript" src="js/bootstrap-dropdown.js"></script></div></div>
+  
+</div>
+    </div>
+	
+	in
+	
+  </div>
+</div>
+
+</body>
+</html>
