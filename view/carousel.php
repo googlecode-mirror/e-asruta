62f81926bd3f1dbb24c40b571afe93f28878901a
rizkynugraha92 <rizkynugraha92@gmail.com>
2012-11-26 16:02:19 +0000

    penambahan script desain penggunaan bootstrap

---
+++ view/carousel.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>  
+<html lang="en">  
+  <head>  
+    <meta charset="utf-8">  
+    <title>Example of carousal with Twitter Bootstrap</title>  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <meta name="description" content="Example of carousal with Twitter Bootstrap version 2.0 from w3resource.com">  
+     <!-- Le styles -->  
+    <link href="twitter-bootstrap-v2/docs/assets/css/bootstrap.css" rel="stylesheet">  
+    <link href="twitter-bootstrap-v2/docs/assets/css/example-fixed-layout.css" rel="stylesheet">  
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->  
+    <!--[if lt IE 9]>  
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>  
+    <![endif]-->  
+  
+    <!-- Le fav and touch icons -->  
+    <link rel="shortcut icon" href="twitter-bootstrap-v2/docs/examples/images/favicon.ico">  
+    <link rel="apple-touch-icon" href="twitter-bootstrap-v2/docs/examples/images/apple-touch-icon.png">  
+    <link rel="apple-touch-icon" sizes="72x72" href="twitter-bootstrap-v2/docs/examples/images/apple-touch-icon-72x72.png">  
+    <link rel="apple-touch-icon" sizes="114x114" href="twitter-bootstrap-v2/docs/examples/images/apple-touch-icon-114x114.png">  
+  </head>  
+  
+  <body>  
+          <div id="myCarousel" class="carousel slide">  
+            <!-- Carousel items -->  
+          <div class="carousel-inner">  
+        <div class="active item"><img src="/update-images/html5_logo.png" alt="HTML5 logo" width="500" height="99" /></div>  
+        <div class="item"><img src="/update-images/javascript-logo.png" alt="JS logo" width="500" height="99" /></div>  
+        <div class="item"><img src="/update-images/schema.png" alt="Schema.org logo" width="500" height="99" /></div>  
+        <div class="item"><img src="/update-images/json.gif" alt="JSON logo" width="500" height="99" /></div>  
+  </div>  
+  <!-- Carousel nav -->  
+  <a class="carousel-control left" href="#myCarousel" data-slide="prev">‹</a>  
+  <a class="carousel-control right" href="#myCarousel" data-slide="next">›</a>  
+</div>  
+</div>  
+</div>  
+  
+    <!-- Le javascript  
+    ================================================== -->  
+    <!-- Placed at the end of the document so the pages load faster -->  
+    <script src="js/jquery.js"></script>  
+    <script src="js/bootstrap-carousel.js"></script>  
+  
+  
+  </body>  
+</html>  
