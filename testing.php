39f8cf6ac02f0b524dc5402cb8e5c9dfe34547d9
djepits5005 <djepits5005@gmail.com>
2012-11-28 09:33:10 +0000

    Perbaikan fungsi dan testing mencari pencarian asisten rumah tangga

--- testing.php
+++ testing.php
@@ -3,7 +3,7 @@ require_once 'config/koneksi.php';
 require_once 'lib/cari.asisten.php';
 require_once 'lib/cariasisten_dao.php';
 
-$cariasisten_dao=new CariAsisten_Dao();
+/*
 $cariasisten=new CariAsisten();
 $cariasisten->keterampilan='Memasak, Mencuci, Merawat Anak';
 $cariasisten->gaji='1000000';
@@ -12,4 +12,9 @@ $cariasisten->jam_kerja='07:00-17:00';
 $cariasisten->luas_rumah='100';
 $cariasisten->anggota_kel='4';
 $cariasisten->lokasi='Surabaya Timur';
+$cariasisten_dao=new CariAsisten_Dao();
 $cariasisten_dao->tambahPencarian($cariasisten);
+*/
+
+$cariasisten_dao=new CariAsisten_Dao();
+$cariasisten_dao->cariPencarian(1);
