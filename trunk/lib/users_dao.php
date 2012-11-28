7de49d56adec7e39241222a4aff70913ebbcd42a
djepits5005@gmail.com <djepits5005@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-11-28 22:37:31 +0000

    Use Case Registrasi dan fungsi validasi untuk registrasi

---
+++ trunk/lib/users_dao.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @author Arif Setiyanto
+ * @copyright 2012
+ */
+ 
+ class Users_Dao{
+	
+	function cekEksistensiUser($username){
+		$koneksi = new Koneksi();
+		
+		$koneksi->pilihkonekdb();
+		
+		$useravailable=false;
+		
+		$sql="
+		select count(username)
+		from users
+		";
+		$hasil=mysql_query($sql);
+		
+		if($hasil<=1){
+			$useravailable=true;
+		}
+		
+		$koneksi->tutupdb();
+		
+		return $useravailable;
+	}
+	
+	function registrasi(Users $user){
+		$koneksi = new Koneksi();
+		
+		$koneksi->pilihkonekdb();
+		
+		$sql="
+		insert
+		into
+		users
+		(
+			username,
+			password,
+			kd_role
+		)
+		values
+		(
+			'".$user->username."',
+			'".$user->password."',
+			'".$user->kd_role."'
+			
+		)
+		";
+		$berhasil=mysql_query($sql);
+		if(!$berhasil){
+			echo "gagal";
+		}
+		$testi->id_testi= mysql_insert_id();
+		
+		$koneksi->tutupdb();
+	}
+ }
