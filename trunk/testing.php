e87534668686ee7db7a2737f29f642874052814d
djepits5005@gmail.com <djepits5005@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-11-28 05:44:35 +0000

    Perbaikan file dao dan testing fungsi cari asisten rumah tangga dengan file testing.php

---
+++ trunk/testing.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'config/koneksi.php';
+require_once 'lib/cari.asisten.php';
+require_once 'lib/cariasisten_dao.php';
+
+$cariasisten_dao=new CariAsisten_Dao();
+$cariasisten=new CariAsisten();
+$cariasisten->keterampilan='Memasak, Mencuci, Merawat Anak';
+$cariasisten->gaji='1000000';
+$cariasisten->hari_kerja='6';
+$cariasisten->jam_kerja='07:00-17:00';
+$cariasisten->luas_rumah='100';
+$cariasisten->anggota_kel='4';
+$cariasisten->lokasi='Surabaya Timur';
+$cariasisten_dao->tambahPencarian($cariasisten);
