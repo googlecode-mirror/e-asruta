23f89aab7d1902563f10ac556392188969d472dd
djepits5005@gmail.com <djepits5005@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-12-08 02:11:48 +0000

    Perubahan struktur folder, perbaikan view halaman_majikan.php dan form_cariasisten.php

--- trunk/view/form_cariasisten.php
+++ trunk/view/form_cariasisten.php
@@ -1,5 +1,5 @@
 <?php
-include 'config/waktu.php';
+include '../config/waktu.php';
 ?>
 
 <!DOCTYPE html PUB
@@ -8,8 +8,8 @@ LIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xh
 <head>
 	<meta name="author" content="Wink Hosting (www.winkhosting.com)" />
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<link rel="stylesheet" href="images/style.css" type="text/css" />
-	<title>e_Asruta</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<title>Form Pencarian Asisten Rumah Tangga</title>
 </head>
 <body>
 	<div id="page" align="center">
@@ -33,26 +33,45 @@ LIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xh
 			<div id="menu" align="right">
 				<div align="left" style="width:189px; height:8px;"><img src="images/mnu_topshadow.gif" width="189" height="8" alt="mnutopshadow" /></div>
 				<div id="linksmenu" align="left">
-					<a href="#" title="Beranda">Beranda</a>
+					<a href="halaman_majikan.php" title="Beranda">Beranda</a>
 					<a href="#" title="Lowongan">Daftar Lowongan Pekerjaan</a>
-					<a href="#" title="Cari Asisten">Pencarian Asisten Pribadi</a>
+					<a href="" title="Cari Asisten">Pencarian Asisten Pribadi</a>
 					<a href="#" title="Testimoni">Testimoni</a>
 				</div>
 				<div align="right" style="width:189px; height:8px;"><img src="images/mnu_bottomshadow.gif" width="189" height="8" alt="mnubottomshadow" /></div>
 			</div>
 		<div id="contenttext">
 			<div class="bodytext" style="padding:12px;" align="justify">
-				<strong>Selamat Datang di Website e-Asruta, penyedia jasa layanan asisten rumah tangga!</strong><br />
-				<br />
-				Website ini menyediakan fasilitas bagi anda untuk mencari asisten rumah tangga sesuai dengan kebutuhan anda
+				<form method="post" action="action.cariasisten.php">
+				<table>
+					<tr>
+						<td>Keterampilan</td><td><input name="keterampilan" type="text" /></td> 
+					</tr>
+					<tr>
+						<td>Gaji yang diinginkan</td><td><input name="gaji" type="text" /></td> 
+					</tr>
+					<tr>
+						<td>Hari kerja dalam seminggu</td><td><input name="hari_kerja" type="text" /></td> 
+					</tr>
+					<tr>
+						<td>Jam kerja dalam sehari</td><td><input name="jam_kerja" type="text" /></td> 
+					</tr>
+					<tr>
+						<td>Luas rumah</td><td><input name="luas_rumah" type="text" /></td> 
+					</tr>
+					<tr>
+						<td>Jumlah anggota keluarga</td><td><input name="anggota_kel" type="text" /></td> 
+					</tr>
+					<tr>
+						<td>Lokasi</td><td><input name="lokasi" type="text" /></td> 
+					</tr>
+					<tr>
+						<td><input name="Submit" type="submit" value="Submit"/></td> 
+					</tr>
+				</table>
+			</form>
 			</div>
-			<div class="panel" align="justify">
-				<span class="orangetitle">Ketentuan Penggunaan Layanan e-Asruta</span>
-				<span class="bodytext"><br />
-				1. Website ini bersifat bebas, <br>
-				2. Tanggung Jawab berada pada pengguna <br>
-				</span></div>
-			</div>
+			
 		</div>
 		<div id="footer" class="smallgraytext" align="center">
 			<a href="#">Beranda</a> | <a href="#">Daftar Lowongan Pekerjaan</a> | <a href="#">Pencarian Asisten Pribadi</a> | <a href="#">Testimoni</a> 
