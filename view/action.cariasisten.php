28a12d7fa62068753271b848e49904c5f6c690c5
djepits5005 <djepits5005@gmail.com>
2012-12-12 16:33:08 +0000

    Issue 6: 	Cari Asisten Rumah Tangga Penyesuaian file logic dan tampilan dengan tabel baru

--- view/action.cariasisten.php
+++ view/action.cariasisten.php
@@ -6,21 +6,25 @@ session_start();
  */
  
 require_once '../config/koneksi.php';
-require_once '../lib/cari.asisten.php';
+require_once '../lib/lowongan.php';
 require_once '../lib/cariasisten_dao.php';
 
 $kd_users=$_SESSION['kduser'];
-//echo $kd_users;
+echo $kd_users;
 
 $cariasisten_dao=new CariAsisten_Dao();
-$cariasisten=new CariAsisten();
-$cariasisten->kd_users=$kd_users;
-$cariasisten->keterampilan=$_POST['keterampilan'];
+$cariasisten=new Lowongan();
+//echo $cariasisten->kd_lowongan;
+
+$cariasisten->pembuat_lamar=$kd_users;
+$cariasisten->kd_jenislo=1;
+$cariasisten->kd_keahlian=$_POST['kd_keahlian'];
 $cariasisten->gaji=$_POST['gaji'];
-$cariasisten->hari_kerja=$_POST['hari_kerja'];
-$cariasisten->jam_kerja=$_POST['jam_kerja'];
-$cariasisten->luas_rumah=$_POST['luas_rumah'];
-$cariasisten->anggota_kel=$_POST['anggota_kel'];
 $cariasisten->lokasi=$_POST['lokasi'];
+$cariasisten->hari_kerja=$_POST['hari_kerja'];
+$cariasisten->menginap=$_POST['menginap'];
+$cariasisten->jam_kerja=$_POST['jam_kerja'];
+
+
 $cariasisten_dao->tambahPencarian($cariasisten);
 header('location:lihat_asisten.php');
