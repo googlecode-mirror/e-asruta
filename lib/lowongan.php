160bb3694ffa64e53e68fd808ea016ceba87cb6d
djepits5005 <djepits5005@gmail.com>
2012-12-12 13:51:14 +0000

    UC 3 sampai dengan UC 10 Penambahan objek file lowongan.php  untuk mengakses tabel lowongan.

---
+++ lib/lowongan.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Arif Setiyanto
+ * @copyright 2012
+ */
+ 
+class Lowongan{
+	var $kd_lowongan;
+	var $pembuat_lamar;
+	var $kd_asisten;
+	var $kd_jenislo;
+	var $kd_keahlian;
+	var $hari_kerja;
+	var $jam_kerja;
+	var $gaji;
+	var $lokasi;
+	var $menginap;
+	
+}
