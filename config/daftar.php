b568bdd20a4d47fc012170ede7bb38ae033853bd
rizkynugraha92 <rizkynugraha92@gmail.com>
2012-12-12 05:22:37 +0000

    daftar sementara, perbaikan menyusul

---
+++ config/daftar.php
@@ -0,0 +1,30 @@
+<form action="submit.php" method="post">
+<table border="0">
+<tbody>
+<tr>
+<td>Masukkan Username</td>
+<td>
+<input name="username" type="text" /></td>
+</tr>
+<tr>
+<td>Masukkan E-mail</td>
+<td>
+<input style="background-color: #ffffa0;" name="email" type="text" /></td>
+</tr>
+<tr>
+<td>Masukkan Password</td>
+<td>
+<input name="pass1" type="password" /></td>
+</tr>
+<tr>
+<td>Ulangi Password</td>
+<td>
+<input name="pass2" type="password" /></td>
+</tr>
+<tr>
+<td></td>
+<td>
+<input name="Submit" type="submit" value="Submit" /></td>
+</tr>
+</tbody></table>
+</form>
