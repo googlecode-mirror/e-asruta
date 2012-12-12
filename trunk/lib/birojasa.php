e82a6db6196a07fe984b91fbe1a4dc05995d03c7
eclipsebeat@gmail.com <eclipsebeat@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-12-12 14:44:30 +0000

    Issue 22, Issue 23, Issue 24

---
+++ trunk/lib/birojasa.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @author Gerhantara
+ * @copyright 2012
+ */
+
+class Birojasa{
+	var $kd_asisten;
+	var $kd_member;
+	var $nm_asisten;
+	var $hapeasisten;
+	var $alamat_asisten;
+	var $tgl_lahirasisten;
+	var $tmpt_lahirasisten;
+	var $kota_asisten;
+	var $no_idasisten;
+    var $copy_asisten;
+}
+
+
+?>
