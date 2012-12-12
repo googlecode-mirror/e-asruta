d2f42f92d78107a7a65ff3f7206607932968aa33
eclipsebeat <eclipsebeat@gmail.com>
2012-12-12 14:44:30 +0000

    Issue 22, Issue 23, Issue 24

---
+++ lib/birojasa_dao.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * @author Gerhantara
+ * @copyright 2012
+ */
+
+
+    class birojasa_dao{
+        
+        function tambah_asisten(birojasa $birojasa, $kd_member){
+        
+        $koneksi = new Koneksi();
+		
+		$koneksi->pilihkonekdb();
+		
+		$sql = "
+			INSERT 
+			INTO
+			asisten
+			(	
+				kd_member,
+				nm_asisten,
+				hapeasisten,
+				alamat_asisten,
+				tgl_lahirasisten,
+				tmpt_lahirasisten,
+				kota_asisten,
+				no_idasisten,
+                copy_asisten
+			)
+			VALUES
+			(
+				'".$birojasa->kd_member."',
+				'".$birojasa->nm_asisten."',
+				'".$birojasa->hapeasisten."',
+				'".$birojasa->alamat_asisten."',
+				'".$birojasa->tgl_lahirasisten."',
+				'".$birojasa->tmpt_lahirasisten."',
+				'".$birojasa->kota_asisten."',
+				'".$birojasa->no_idasisten."',
+                '".$birojasa->copy_asisten."'
+			)
+		" ;
+		$berhasil=mysql_query($sql);
+		if(!$berhasil){
+			echo "gagal";
+		}
+		
+		$birojasa->kd_member = mysql_insert_id();
+		
+		$koneksi->tutupdb();
+	
+	}
+        }
+        
+        function hapus_asisten($kd_asisten){
+            $koneksi = new Koneksi();
+            $koneksi->pilihkonekdb();
+		
+    		$sql = "
+    		DELETE
+    		FROM
+    		asisten
+    		WHERE
+    		kd_asisten = '".$kd_asisten."'
+    		";
+		
+		mysql_query($sql);
+		
+		$koneksi->tutupdb();
+        }
+    
+        function edit_asisten(birojasa $birojasa){
+            $koneksi = new Koneksi();
+            $$koneksi->pilihkonekdb();
+            
+            $sql = "
+            Update
+            asisten set
+            kd_member = '".$birojasa->kd_member."',
+            nm_asisten = '".$birojasa->nm_asisten."',
+            hapeasisten = '".$birojasa->hapeasisten."',
+            alamat_asisten = '".$birojasa->alamat_asisten."',
+            tgl_lahirasisten = '".$birojasa->tgl_lahirasisten."',
+            tmpt_lahirasisten = '".$birojasa->tmpt_lahirasisten."',
+            kota_asisten = '".$birojasa->kota_asisten."',
+            no_idasisten = '".$birojasa->no_idasisten."',
+            copy_asisten = '".$birojasa->copy_asisten."'";
+        }
+
+
+?>
