9ad70bfae7ff535faef7ff27d7dfd5fca28d65c2
djepits5005 <djepits5005@gmail.com>
2012-12-12 13:47:01 +0000

    Issue 12: 	Use Case Login Perbaikan Use case login. Untuk kode role distandarkan 1 untuk admin, 2 untuk birojasa, 3 untuk majikan. Karena role menentukan halaman yang dituju setelah login

--- config/auth.php
+++ config/auth.php
@@ -6,25 +6,25 @@
 			$koneksi->pilihkonekdb();
 			$koderole='';
 			
-			$sql="select kd_users,kd_role from users where username='".$username."' and password='".$password."'";
+			$sql="select KD_USER, KD_ROLES from users where USERNAME='".$username."' and PASSWORD='".$password."'";
 			$search=mysql_query($sql);
 			$role =mysql_fetch_array($search);
 		//	print_r($role);
 			while($role){
-				$kd_user=$role['kd_users'];
+				$kd_user=$role['KD_USER'];
 				$_SESSION['kduser']=$kd_user;
 				$_SESSION['users']=$username;
 				$_SESSION['pass']=$password;
 				
-				if ($role['kd_role']==1){
+				if ($role['KD_ROLES']==1){
 					return $this->koderole="admin";
 				}
 				
-				if ($role['kd_role']==2){
+				if ($role['KD_ROLES']==2){
 					return $this->koderole="birojasa";	
 				}
 				
-				if ($role['kd_role']==3){
+				if ($role['KD_ROLES']==3){
 					return $this->koderole="majikan";
 				}
 			}
