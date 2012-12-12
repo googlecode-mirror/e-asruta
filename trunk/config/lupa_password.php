c260236256860af4911d21fbf10848dcb8de4492
rizkynugraha92@gmail.com <rizkynugraha92@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-12-12 05:25:04 +0000

    perbaikan menyusul

---
+++ trunk/config/lupa_password.php
@@ -0,0 +1,15 @@
+<form action="kirimpassword.php" method="post">
+<table border="0">
+<tbody>
+<tr>
+<td>Masukkan Username Anda</td>
+<td>
+<input name="username" type="text" /></td>
+</tr>
+<tr>
+<td></td>
+<td>
+<input name="Submit" type="submit" value="Submit" /></td>
+</tr>
+</tbody></table>
+</form>
