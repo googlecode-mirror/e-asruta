58306d8cad8889dd98fa222c69b76353b2663405
djepits5005 <djepits5005@gmail.com>
2012-12-19 04:45:28 +0000

    Issue 6: 	Cari Asisten Rumah Tangga Penggabungan file form_cariasisten.php dan file action action.cariasisten.php, serta penambahan validasi isian form

--- view/form_cariasisten.php
+++ view/form_cariasisten.php
@@ -3,22 +3,48 @@ include_once 'header.php';
 include_once 'sidebar.php';
 include '../config/waktu.php';
 include '../lib/cariasisten_dao.php';
-include '../lib/cari.asisten.php';
+require_once '../lib/lowongan.php';
+?>
+<div id="contenttext">
+<?php
+$kd_users=$_SESSION['kduser'];
+//echo $kd_users;
+
 $cari=new CariAsisten_Dao();
+$cariasisten=new Lowongan();
+
 $data=$cari->cari_keahlian();
+//echo $cariasisten->kd_lowongan;
+if($_POST){
+$cariasisten->pembuat_lamar=$kd_users;
+$cariasisten->kd_jenislo=1;
+$cariasisten->kd_keahlian=$_POST['kd_keahlian'];
+$cariasisten->gaji=$_POST['gaji'];
+$cariasisten->lokasi=$_POST['lokasi'];
+$cariasisten->hari_kerja=$_POST['hari_kerja'];
+$cariasisten->menginap=$_POST['menginap'];
+$cariasisten->jam_kerja=$_POST['jam_kerja'];
+	if(empty($cariasisten->pembuat_lamar) || empty($cariasisten->kd_keahlian) || empty($cariasisten->gaji) || 
+		empty($cariasisten->lokasi) || empty($cariasisten->hari_kerja) || empty($cariasisten->menginap) || empty($cariasisten->jam_kerja)){
+		echo "Semua data harus diisi";
+	}else{
+		$cari->tambahPencarian($cariasisten);
+		header('location:lihat_asisten.php');
+	}
+}
 ?>
 
-		<div id="contenttext">
+		
 			<div class="bodytext" style="padding:12px;" align="justify">
-				<form method="post" action="action.cariasisten.php">
+				<form method="post" action="">
 				<table>
 					<tr>
 						<td>Keterampilan</td>
 						<td>
-							<select name="keterampilan">
-								<?php
-								if($data!=NULL){
-								foreach ($data as $cari){?>	
+							<select name="kd_keahlian">
+							<?php
+							if($data!=NULL){
+							foreach ($data as $cari){?>	
 							<option value="<?php echo $cari->kd_keahlian;?>"><?php echo $cari->jns_keahlian; ?></option>
 							<?php } 
 							}?>
@@ -38,7 +64,7 @@ $data=$cari->cari_keahlian();
 					<tr>
 						<td>Menginap tidak?</td>
 						<td>
-							<select name="lokasi"><option value="Ya">Ya</option> <option value="Tidak">Tidak</option></select></td>
+							<select name="menginap"><option value="Ya">Ya</option> <option value="Tidak">Tidak</option></select></td>
 					</tr>
 					<tr>
 						<td>Jam Kerja</td><td><input name="jam_kerja" type="text" /></td> 
