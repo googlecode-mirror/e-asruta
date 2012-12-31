1fe8cdab893076de440bc44bd5368f96b11e667e
djepits5005@gmail.com <djepits5005@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2012-12-31 04:32:24 +0000

    Issue 26: 	Isi Testimoni Pembuatan action form dan perbaikan lihat testimoni

--- trunk/view/rekam_testimoni.php
+++ trunk/view/rekam_testimoni.php
@@ -2,14 +2,32 @@
 include_once 'header.php';
 include_once 'sidebar.php';
 include '../config/waktu.php';
+include '../lib/testimoni.php';
+include '../lib/testimoni_dao.php';
+$testimoni=new Testimoni();
+$testi=new Testimoni_Dao();
 ?>
 
 		<div id="contenttext">
+		<?php
+				$kd_users=$_SESSION['kduser'];
+				//echo $kd_users;
+				if($_POST){
+				$testimoni->kd_member=$kd_users;
+				$testimoni->isi_testi=$_POST['testimoni'];
+				if(empty($testimoni->isi_testi)){
+						echo "Anda belum mengisi testimoni";
+					}else{
+						$testi->isiTestimoni($testimoni);
+						header('location:lihat_testimoni.php');
+					}
+				}
+		?>
 			<div class="bodytext" style="padding:12px;" align="justify">
-				<form method="post" action="action.cariasisten.php">
+				<form method="post" action="">
 				<table>
 					<tr>
-						<td>Isi Testimoni</td><td><input name="testimoni" type="textarea" /></td> 
+						<td>Isi Testimoni</td><td><input name="testimoni" type="text" /></td> 
 					</tr>
 					<tr>
 						<td></td>
