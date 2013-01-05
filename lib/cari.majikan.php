45d5468c92e44f73e7eb56510b4736852a1504e4
eclipsebeat <eclipsebeat@gmail.com>
2013-01-05 11:21:17 +0000

    perbaikan script untuk issue 18 mencari lowongan pekerjaan

--- lib/cari.majikan.php
+++ lib/cari.majikan.php
@@ -6,9 +6,12 @@
  
 class CariMajikan{
 	var $kd_lowongan;
+	var $nm_member;
 	var $pembuat_lamar;
     var $kd_jenislo;
+	var $jenis_lowongan;
 	var $kd_keahlian;
+	var $jns_keahlian;
 	var $gaji;
 	var $hari_kerja;
 	var $jam_kerja;
--- lib/carimajikan_dao.php
+++ lib/carimajikan_dao.php
@@ -8,19 +8,19 @@
     class CariMajikan_Dao{
         
         
-        function daftar_lo(){
+        function daftar_lo($halaman,$limit){
     	
     		$koneksi = new Koneksi();
     		$koneksi->pilihkonekdb();
     		
     		$sql = "
     		SELECT 
-    		b.jenis_lowongan,
-    		a.pembuat_lamar,
-    		c.jns_keahlian,
+			d.nm_member,
+    		b.jenis_lowongan as lowongan,
+    		c.jns_keahlian as keahlian,
+			a.hari_kerja,
     		a.jam_kerja,
     		a.menginap,
-    		a.hari_kerja,
             a.lokasi,
             a.gaji
     		FROM
@@ -29,41 +29,36 @@
     		keahlian as c
             JOIN
             jenis_lowongan as b
-            
-            
+			JOIN
+			members as d
     		WHERE
-            
             a. kd_jenislo = b.kd_jenislo
-            
             AND
-            
             a.kd_keahlian = c.kd_keahlian
- 
+			ORDER BY kd_lowongan ASC
+			LIMIT $halaman,$limit
     		";
     		
-    		$cari = false;
-    		
-    		$res = mysql_query($sql);
-    		
-    		if($res){
-    		
-    			$row = mysql_fetch_assoc($res);
-    		
-    			$cari = new CariMajikan();
-    			$cari->jenis_lowongan=$row['jenis_lowongan'];
-    			$cari->pembuat_lamar=$row['pembuat_lamar'];
-    			$cari->jns_keahlian=$row['jns_keahlian'];
-    			$cari->jam_kerja=$row['jam_kerja'];
-    			$cari->menginap=$row['menginap'];
-    			$cari->hari_kerja=$row['hari_kerja'];
-                $cari->lokasi=$row['lokasi'];
-    			$cari->gaji=$row['gaji'];		
-    		
-    		}
-    		
-    		$koneksi->tutupdb();
-    		
-    		return $cari;
+    		$list_cari = array();
+			$res = mysql_query($sql);
+			if($res){
+				while($row = mysql_fetch_assoc($res)){
+					$cari = new CariMajikan();
+					$cari->nm_member=$row['nm_member'];
+					$cari->jenis_lowongan=$row['lowongan'];
+					$cari->jns_keahlian=$row['keahlian'];
+					$cari->hari_kerja=$row['hari_kerja'];
+					$cari->jam_kerja=$row['jam_kerja'];
+					$cari->menginap=$row['menginap'];
+					$cari->lokasi=$row['lokasi'];
+					$cari->gaji=$row['gaji'];		
+					
+					$list_cari[] = $cari;
+				}
+			}
+		
+		$koneksi->tutupdb();
+		return $list_cari;
     	}
         
         public function cari_khusus(){
@@ -140,6 +135,21 @@
     		
     		return $cari;
         }
+		
+		function hitungHalaman() {
+		$koneksi = new Koneksi();
+		$koneksi->pilihkonekdb();
+		$sql="
+			SELECT 
+			count(*) as total 
+			FROM 
+			lowongan 
+		";
+		
+		$itung = mysql_query($sql);
+		$jumlah = mysql_fetch_array($itung);
+		return $jumlah['total'];
+	}
 	
     }
 
