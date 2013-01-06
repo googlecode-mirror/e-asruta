71cf828cb8147ce78c3e433f089ff6113087e2cd
rizkynugraha92@gmail.com <rizkynugraha92@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2013-01-06 14:35:42 +0000

    memperbaiki use case lupa_password (sukses)

--- trunk/config/lupa_password.php
+++ trunk/config/lupa_password.php
@@ -1,3 +1,48 @@
+<!--/**
+ * @author rizky
+ * @copyright 2012
+ */-->
+ <!--/**
+ * @author rizky
+ * @copyright 2012
+ */-->
+ <?php
+include 'waktu.php';
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="author" content="Wink Hosting (www.winkhosting.com)" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<link rel="stylesheet" href="../view/bootstrap/css/bootstrap.css" type="text/css" />
+	<title>regis_asisten</title>
+	</head>
+	<body>
+	<div id="page" align="center">
+		<div id="toppage" align="center">
+			<div id="date">
+				<div class="smalltext" style="padding:13px;"><strong><?php echo $waktunya; ?></strong></div>
+			</div>
+			<div id="topbar">
+				<div align="right" style="padding:12px;" class="smallwhitetext"></div>
+			</div>
+		</div>
+		<div id="header" align="center">
+			<div class="titletext" id="logo">
+				<div class="logotext" style="margin:30px">e_As<span class="orangelogotext">ru</span>ta</div> 
+			</div>
+			<div id="pagetitle">
+				<div id="title" class="titletext" align="right">Website e-Asruta</div>
+			</div>
+		</div>
+		<div id="content" align="left">
+			<div id="menu" align="right">
+			</div>
+		<div id="contenttext2">
+		  <div class="bodytext" style="padding:12px;" align="justify">
+						  
+					
 <form action="kirimpassword.php" method="post">
 <table border="0">
 <tbody>
