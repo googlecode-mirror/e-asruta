4d5408b47073219d67824deebb0561e9e0d5f33f
eclipsebeat@gmail.com <eclipsebeat@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2013-01-06 08:30:22 +0000

    perbaikan halaman tampilan untuk melamar lowongan (issue 20)

--- trunk/view/lamar_lowongan.php
+++ trunk/view/lamar_lowongan.php
@@ -11,50 +11,41 @@ $kd_lowongan=mysql_real_escape_string($id);
 
 ?>
 
-<!DOCTYPE html> 
-<html lang="en"> 
-<head> 
-	<meta charset="utf-8"> 
-	<title>Twitter Bootstrap Version2.0 search form layout example</title> 
-		<meta name="description" content="Twitter Bootstrap Version2.0 search form layout example from w3resource.com."> 
-		<link href="twitter-bootstrap-v2/docs/assets/css/bootstrap.css" rel="stylesheet">
-			</head>
-				<body>
-				<table border="2" align="center" class="table table-striped">
-					<tr>
-						<td>Pembuat Lowongan</td>
-						<td>Jenis Lowongan</td>
-						<td>Keahlian</td>
-						<td>Hari Kerja</td>
-						<td>Jam Kerja</td>
-						<td>Menginap Tidak?</td> 
-						<td>Lokasi</td>
-						<td>Gaji</td>
-					</tr>
+	<div id="contenttext">
+		<div class="bodytext" style="padding:12px;" align="justify">
+			<table border="2" align="center" class="table table-striped">
+				<tr>
+					<td>Pembuat Lowongan</td>
+					<td>Jenis Lowongan</td>
+					<td>Keahlian</td>
+					<td>Hari Kerja</td>
+					<td>Jam Kerja</td>
+					<td>Menginap Tidak?</td> 
+					<td>Lokasi</td>
+					<td>Gaji</td>
+				</tr>
 					
-					<?php
-					$data = $lowongan->daftar_low($kd_lowongan);
-//					echo $mulai;
-//					echo $pageSize; 
-					if($data!=NULL){
-						foreach ($data as $cari){
+				<?php
+				$data = $lowongan->daftar_low($kd_lowongan);
+				if($data!=NULL){
+					foreach ($data as $cari){
 				?>				
-				   <tr>	<td><?php echo $cari->nm_member; ?></td>
-						<td><?php echo $cari->jenis_lowongan;?></td>
-						<td><?php echo $cari->jns_keahlian; ?></td>
-						<td><?php echo $cari->hari_kerja; ?></td>
-						<td><?php echo $cari->jam_kerja; ?></td>
-						<td><?php echo $cari->menginap; ?></td>
-						<td><?php echo $cari->lokasi; ?></td>
-						<td><?php echo $cari->gaji; ?></td>
-						</tr>
+				<tr><td><?php echo $cari->nm_member; ?></td>
+					<td><?php echo $cari->jenis_lowongan;?></td>
+					<td><?php echo $cari->jns_keahlian; ?></td>
+					<td><?php echo $cari->hari_kerja; ?></td>
+					<td><?php echo $cari->jam_kerja; ?></td>
+					<td><?php echo $cari->menginap; ?></td>
+					<td><?php echo $cari->lokasi; ?></td>
+					<td><?php echo $cari->gaji; ?></td>
+				</tr>
 						
 				<?php 	}
 					} ?>
 			</table>
 			<fieldset>
 			<div class="control-group">  
-            <label class="control-label" for="select01">Select list</label>  
+            <label class="control-label" for="select01">Asisten Yang Akan Di Lamarkan</label>  
             <div class="controls">  
               <select id="select01">  
                 <?php 
@@ -68,8 +59,7 @@ $kd_lowongan=mysql_real_escape_string($id);
             <button class="btn">Cancel</button>  
           </div>  
 		</fieldset>
-		</body>
-</html>
-            
+		</div>
+		</div>
 
 <?php include 'footer.php'; ?>
