70836774b4d190855aa32bfc10880937dd2878b2
eclipsebeat@gmail.com <eclipsebeat@gmail.com@d60e5b0a-78f7-22c8-39a6-65714f1eadb7>
2013-01-06 16:24:13 +0000

    perbaikan script untuk issue 22 (tambah_asisten) dan issue 24 (edit_asisten)

--- trunk/lib/birojasa_dao.php
+++ trunk/lib/birojasa_dao.php
@@ -72,28 +72,33 @@
 		$koneksi->tutupdb();
         }
     
-        function edit_asisten($id_asisten){
+        function edit_asisten(Birojasa $yy, $kd_asisten){
             $koneksi = new Koneksi();
             $koneksi->pilihkonekdb();
-			
-			$birojasa = new birojasa();
             
             $sql = "
             Update
             asisten set
-            nm_asisten = '".$birojasa->nm_asisten."',
-            hapeasisten = '".$birojasa->hapeasisten."',
-            alamat_asisten = '".$birojasa->alamat_asisten."',
-            tgl_lahirasisten = '".$birojasa->tgl_lahirasisten."',
-            tmpt_lahirasisten = '".$birojasa->tmpt_lahirasisten."',
-            kota_asisten = '".$birojasa->kota_asisten."',
-            no_idasisten = '".$birojasa->no_idasisten."',
-            copy_asisten = '".$birojasa->copy_asisten."'
-			where kd_asisten = ".$id_asisten."'";
+            nm_asisten = '".$yy->nm_asisten."',
+            hapeasisten = '".$yy->hapeasisten."',
+            alamat_asisten = '".$yy->alamat_asisten."',
+            tgl_lahirasisten = '".$yy->tgl_lahirasisten."',
+            tmpt_lahirasisten = '".$yy->tmpt_lahirasisten."',
+            kota_asisten = '".$yy->kota_asisten."',
+            no_idasisten = '".$yy->no_idasisten."',
+            copy_asisten = '".$yy->copy_asisten."'
+			where kd_asisten = ".$kd_asisten."'";
 			
-			mysql_query($sql);
+			$berhasil=mysql_query($sql);
+		if($berhasil){
+			echo "yes, Berhasil";
+		} else {
+			echo "ngga berhasil :(";
+		}
 		
-			$koneksi->tutupdb();
+		$birojasa->kd_asisten = mysql_insert_id();
+		
+		$koneksi->tutupdb();
         }
 		
 		function daftar_asisten($kd_member){
@@ -129,6 +134,52 @@
 					$cari->kota_asisten=$row['kota_asisten'];
 					$cari->tmpt_lahirasisten=$row['tmpt_lahirasisten'];
 					$cari->tgl_lahirasisten=$row['tgl_lahirasisten'];
+					
+					$list_cari[] = $cari;
+				}
+			}
+		
+		$koneksi->tutupdb();
+		return $list_cari;
+	}
+	
+	function data_asisten($kd_asisten){
+			$koneksi = new Koneksi();
+			
+			$koneksi->pilihkonekdb();
+            
+            $sql = "
+			Select
+			kd_asisten,
+			nm_asisten,
+			hapeasisten,
+			alamat_asisten,	
+			kota_asisten,		
+			tmpt_lahirasisten,
+			tgl_lahirasisten,
+			no_idasisten,
+			copy_asisten
+			From
+			asisten
+			Where
+			kd_asisten = '".$kd_asisten."'
+			";
+			
+			$list_cari = false;
+		
+			$res = mysql_query($sql);
+			if($res){
+				while($row = mysql_fetch_assoc($res)){
+					$cari = new birojasa();
+					$cari->kd_asisten=$row['kd_asisten'];
+					$cari->nm_asisten=$row['nm_asisten'];
+					$cari->hapeasisten=$row['hapeasisten'];
+					$cari->alamat_asisten=$row['alamat_asisten'];
+					$cari->kota_asisten=$row['kota_asisten'];
+					$cari->tmpt_lahirasisten=$row['tmpt_lahirasisten'];
+					$cari->tgl_lahirasisten=$row['tgl_lahirasisten'];
+					$cari->no_idasisten=$row['no_idasisten'];
+					$cari->copy_asisten=$row['copy_asisten'];
 					
 					$list_cari[] = $cari;
 				}
--- trunk/view/form_editasisten.php
+++ trunk/view/form_editasisten.php
@@ -7,66 +7,69 @@ require_once '../lib/birojasa.php';
 ?>
 <div id="contenttext">
 <?php
-$kd_member = $_SESSION['kduser'];
+//$kd_member = $_SESSION['kduser'];
 //echo $kd_users;
 
 $edit = new birojasa_Dao();
-$birojasa = new birojasa();
+$yy = new birojasa();
 //echo $_GET['id'];
 $id = $_GET['id'];
-$id_asisten=mysql_real_escape_string($id);
+$kd_asisten=mysql_real_escape_string($id);
 
-$data=$edit->edit_asisten($id_asisten);
+$data=$edit->data_asisten($kd_asisten);
 //echo $birojasa->kd_lowongan;
 if($_POST){
-$birojasa->kd_asisten=$kd_asisten;
-$birojasa->nm_asisten=$_POST['nm_asisten'];
-$birojasa->hapeasisten=$_POST['hapeasisten'];
-$birojasa->alamat_asisten=$_POST['alamat_asisten'];
-$birojasa->tgl_lahirasisten=$_POST['tgl_lahirasisten'];
-$birojasa->tmpt_lahirasisten=$_POST['tmpt_lahirasisten'];
-$birojasa->kota_asisten=$_POST['kota_asisten'];
-$birojasa->no_idasisten=$_POST['no_idasisten'];
-$birojasa->copy_asisten=$_POST['copy_asisten'];
-	if(empty($birojasa->nm_asisten) || empty($birojasa->hapeasisten) || empty($birojasa->alamat_asisten) || 
-		empty($birojasa->tgl_lahirasisten) || empty($birojasa->tmpt_lahirasisten) 
-		|| empty($birojasa->kota_asisten) || empty($birojasa->no_idasisten) || empty($birojasa->copy_asisten)){
+$yy->kd_asisten=$kd_asisten;
+$yy->nm_asisten=$_POST['nm_asisten'];
+$yy->hapeasisten=$_POST['hapeasisten'];
+$yy->alamat_asisten=$_POST['alamat_asisten'];
+$yy->tgl_lahirasisten=$_POST['tgl_lahirasisten'];
+$yy->tmpt_lahirasisten=$_POST['tmpt_lahirasisten'];
+$yy->kota_asisten=$_POST['kota_asisten'];
+$yy->no_idasisten=$_POST['no_idasisten'];
+$yy->copy_asisten=$_POST['copy_asisten'];
+	if(empty($yy->nm_asisten) || empty($yy->hapeasisten) || empty($yy->alamat_asisten) || 
+		empty($yy->tgl_lahirasisten) || empty($yy->tmpt_lahirasisten) 
+		|| empty($yy->kota_asisten) || empty($yy->no_idasisten)){
 		echo "Semua data harus diisi";
 	}else{
-		$tambah->edit_asisten($birojasa);
+		$edit->edit_asisten($yy, $kd_asisten);
 		header('location:daftar_asisten.php');
 	}
 }
 ?>
-
+		<?php
+					if($data!=NULL){
+						foreach ($data as $list){
+				?>
 		
 			<div class="bodytext" style="padding:12px;" align="justify">
 				<form method="post" action="">
 				<table>
 					<tr>
-						<td>Nama Asisten</td><td><input name="nm_asisten" type="text" value="<?php echo $birojasa->nm_asisten ?>" /></td>
+						<td>Nama Asisten</td><td><input name="nm_asisten" type="text" value="<?php echo $list->nm_asisten ?>" /></td>
 					</tr>
 					<tr>
-						<td>Nomor Handphone Asisten</td><td><input name="hapeasisten" type="text" value="<?php echo $birojasa->hapeasisten ?>" /></td> 
+						<td>Nomor Handphone Asisten</td><td><input name="hapeasisten" type="text" value="<?php echo $list->hapeasisten ?>" /></td> 
 					</tr>
 					<tr>
-						<td>Alamat Asisten</td><td><input name="alamat_asisten" type="text" value="<?php echo $birojasa->alamat_asisten ?>" /></td> 
+						<td>Alamat Asisten</td><td><input name="alamat_asisten" type="text" value="<?php echo $list->alamat_asisten ?>" /></td> 
 					</tr>
 					<tr>
-						<td>Tanggal Lahir Asisten</td><td><input name="tgl_lahirasisten" type="text" value="<?php echo $birojasa->tgl_lahirasisten ?>" /></td> 
+						<td>Kota</td><td><input name="kota_asisten" type="text" value="<?php echo $list->kota_asisten ?>" /></td> 
+					</tr>
+					<tr>
+						<td>Tempat Lahir Asisten</td><td><input name="tmpt_lahirasisten" type="text" value="<?php echo $list->tmpt_lahirasisten ?>" /></td> 
+					</tr>
+					<tr>
+						<td>Tanggal Lahir Asisten</td><td><input name="tgl_lahirasisten" type="text" value="<?php echo $list->tgl_lahirasisten ?>" /></td> 
 					</tr>
 					</tr>
 					<tr>
-						<td>Tempat Lahir Asisten</td><td><input name="tmpt_lahirasisten" type="text" value="<?php echo $birojasa->tmpt_lahirasisten ?>" /></td> 
+						<td>Nomor ID Card (KTP / SIM)</td><td><input name="no_idasisten" type="text" value="<?php echo $list->no_idasisten ?>" /></td> 
 					</tr>
 					<tr>
-						<td>Kota</td><td><input name="kota_asisten" type="text" value="<?php echo $birojasa->kota_asisten ?>" /></td> 
-					</tr>
-					<tr>
-						<td>Nomor ID Card (KTP / SIM)</td><td><input name="no_idasisten" type="text" value="<?php echo $birojasa->no_idasisten ?>" /></td> 
-					</tr>
-					<tr>
-						<td>Gambar kopi ID Card</td><td><input name="copy_asisten" type="image" value="<?php echo $birojasa->copy_asisten ?>" /></td> 
+						<td>Gambar kopi ID Card</td><td><input name="copy_asisten" type="image" value="<?php echo $list->copy_asisten ?>" /></td> 
 					</tr>
 					<tr>
 						<td></td>
@@ -77,5 +80,7 @@ $birojasa->copy_asisten=$_POST['copy_asisten'];
 				</table>
 			</form>
 			</div>
+			<?php 	}
+					} ?>
 		</div>	
 <?php include 'footer.php'; ?>
